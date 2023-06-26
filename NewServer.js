5dbc123601edbdb101efa9518ae28e4639b9d37a
Nooblet07 <carlow2010@gmail.com>
2023-06-26 12:02:55 -0400

    Created

---
+++ NewServer.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const HOST = 'localhost';
+const PORT = 3000;
+
+const server = http.createServer((request, response) => {
+    if (request.method === 'GET') {
+        response.end('Hello Galvanize!')
+    }
+})
+
+server.listen(PORT, HOST, () => {
+    console.log(`Server is running on ${HOST}:${PORT}`)
+})
